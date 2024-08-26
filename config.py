d8d9f9474197c20c08fcd78ad5b522ea151d87da
AHua <119821922+UstinAna@users.noreply.github.com>
2024-08-26 19:25:56 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-name = "金融骗术种类"
-name = input("Enter the name of your pdf: ")
+name = "Title of the pdf" # the name of the final pdf/doc that's gonna be outputted
+zoom = 3 # Zoom factor, can be adjusted to improve quality
